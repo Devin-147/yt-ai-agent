a5413a856bbba181aac2b6234e5c7cb77fdc5668
Devin-147 <devinscannura@icloud.com>
2026-01-03 15:37:37 -0500

    Simplify Tailwind config by removing color extensions
    Removed custom color extensions for purple and pink.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,20 +5,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        purple: {
-          400: "#c084fc",
-          600: "#9333ea",
-          700: "#7c3aed",
-          900: "#4c1d95"
-        },
-        pink: {
-          400: "#f472b6",
-          600: "#ec4899"
-        }
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
